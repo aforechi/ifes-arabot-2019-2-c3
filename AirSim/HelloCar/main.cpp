2461cdf1e441b2315fa4c543bfe010771ac6e677
Andre Nunes <57664802+andreljn@users.noreply.github.com>
2019-12-11 13:39:00 -0300

    316

--- AirSim/HelloCar/main.cpp
+++ AirSim/HelloCar/main.cpp
@@ -99,8 +99,8 @@ int main()
 {
 	msr::airlib::CarRpcLibClient simulador;
 	Waypoints checkpoints, trajectory;
-	LateralControl lateral_control(20.0, 1.0, 9.0);
-	LongitudinalControl velocity_control(1.0, 0.5, 0.01);
+	LateralControl lateral_control(15.0, 1.0, 9.0);
+	LongitudinalControl velocity_control(1.0, 0.0, 0.01);
 
 	int opcao;
 	std::cout << "Favor digite a opcao de controle:\n";
